54ab6c1671a4b301b22ba3c9c2e51f84806bc941
congchenutd <congchenutd@36a2527f-5186-b56b-cd2d-6a67c257459d>
2012-03-07 03:34:02 +0000

    = before refactoring

--- trunk/TestLoader.cpp
+++ trunk/TestLoader.cpp
@@ -118,8 +118,8 @@ TestPage* TestLoader::loadQuestion()
 
 	// attributes of the question
 	QString title   = xml.attributes().value("title")  .toString();
-	QString start   = xml.attributes().value("start")  .toString();
-	QString end     = xml.attributes().value("end")    .toString();
+//	QString start   = xml.attributes().value("start")  .toString();
+//	QString end     = xml.attributes().value("end")    .toString();
 	bool    maySkip = xml.attributes().value("mayskip").toString() == "true";
 	bool    timeIt  = xml.attributes().value("timeit") .toString() == "true";
 	bool    isName  = xml.attributes().value("isname") .toString() == "true";
@@ -134,14 +134,15 @@ TestPage* TestLoader::loadQuestion()
 		return 0;
 	if(!xml.readNextStartElement())
 		return 0;
-	mainWindow->setAnswered(true);
+	mainWindow->setAnswered(true);   // ??
+
+	// prepare the page with the question and the answer area
+	QString pageName = xml.name().toString();
 
 	// attributes of answers
 	int min = xml.attributes().value("min").toString().toInt();
 	int max = xml.attributes().value("max").toString().toInt();
 
-	// prepare the page with the question and the answer area
-	QString pageName = xml.name().toString();
 	TestPage* page = createPage(pageName);
 	page->setTitle(title);
 	page->setText(content);
@@ -149,8 +150,8 @@ TestPage* TestLoader::loadQuestion()
 	page->setSkippable(maySkip);
 	page->setTimerEnabled(timeIt);
 	page->setIsName(isName);
-	page->setDuration(QTime::fromString(start, "hh:mm:ss"),
-					  QTime::fromString(end,   "hh:mm:ss"));
+//	page->setDuration(QTime::fromString(start, "hh:mm:ss"),
+//					  QTime::fromString(end,   "hh:mm:ss"));
 	if(pageName == "single" || pageName == "multiple") {
 		while(xml.readNextStartElement() && xml.name() == "choice")
 			page->addChoice(xml.readElementText());
